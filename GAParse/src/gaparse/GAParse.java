67ad4e6dc02b739fb4574dc68e10b7bd6ecf00ee
xrewndel <xrew@yandex.ru>
2014-09-07 19:12:15 +0400

    update for new format of file and logic

--- GAParse/src/gaparse/GAParse.java
+++ GAParse/src/gaparse/GAParse.java
@@ -25,7 +25,7 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class GAParse {
     private final static Logger log = Logger.getLogger(GAParse.class.getSimpleName());
-    private static String path = "D:\\projects\\ga\\dist\\log";
+    private static String path = "";
     private static List<Stat> stats = new LinkedList<>();
     Map s;
     
@@ -38,30 +38,32 @@ public class GAParse {
         int bestfr = fr;
         int bestwr = wr;
         String bestFile = "";
+        boolean oldType = false;
         // set settings for log
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
         System.setProperty("current.date", dateFormat.format(new Date()));
+        // set path
+        if (args.length > 1) path = args[1];
+        // лучше называть каталог с результатами осмысленно :)
+        String dir = path.substring(path.lastIndexOf("\\") + 1).replace("_", "");
+        System.setProperty("mydir", dir);
         File cfgFile = new File(args[0]);
         PropertyConfigurator.configure(cfgFile.getAbsolutePath());
         
-        // set path
-        if (args.length > 1) path = args[1];
-
         File[] files = getFilesInDir(path);
         for (File file : files) {
-            //log.info("#" + files[i].getName().indexOf("_"));
             log.info("File: " + file.getName());
             String[] t = file.getName().substring(23).replaceAll(".log", "").split("-");
             for (String t1 : t) {
                 String[] q = t1.split("_");
                 switch(q[0]) {
-                    case "c": cross = Integer.valueOf(q[1]);    break;
+                    case "c": cross = Integer.valueOf(q[1]);        break;
                     case "cross": cross = Integer.valueOf(q[1]);    break;
-                    case "m": mutate = Integer.valueOf(q[1]);   break;
-                    case "mutate": mutate = Integer.valueOf(q[1]);   break;
-                    case "fr": fr = Integer.valueOf(q[1]);      break;
-                    case "wr": wr = Integer.valueOf(q[1]);      break;
-                    default: System.out.println("Param " + q[0] + " is uknown");
+                    case "m": mutate = Integer.valueOf(q[1]);       break;
+                    case "mutate": mutate = Integer.valueOf(q[1]);  break;
+                    case "fr": fr = Integer.valueOf(q[1]);          break;
+                    case "wr": wr = Integer.valueOf(q[1]);          break;
+                    default: System.out.println("Param " + q[0] + " is unknown");
                 }
             }
             log.info("Cross: " + cross);
@@ -69,48 +71,45 @@ public class GAParse {
             log.info("Free rate: " + fr);
             log.info("Waste rate: " + wr);
             
-            
             // среднее считать - за сколько поколений в среднем достигается максимальнй фитнес - проще в расчетах писать и пихать в файл
             try {
                 Map map = read(file.getAbsolutePath());
                 log.info("Map: " + map);
                 List<Double> fitness = (List<Double>) map.get("fitness");
                 log.info("Fitness: " + fitness);
+                if (fr == -1) {
+                    oldType = true;
+                    double d = (double) map.get("fr");
+                    fr = (int) (d * 100);
+                    log.info("Free rate 2: " + fr);
+                    double d2 = (double) map.get("wr");
+                    wr = (int) (d2 * 100);
+                    log.info("Waste rate 2: " + wr);
+                } 
+                
                 for (Double fit : fitness) {
                     if (fit > bestFitness) {
                         bestFitness = fit;
-                        bestFile = file.getAbsolutePath();
-                        if (fr == -1) {
-                            double d = (double) map.get("fr");
-                            System.out.println("d1: " + d);
-                            //bestfr = (int) map.get("fr");
-                            bestfr = (int) (d * 100);
-                            System.out.println("bestfr: " + bestfr);
-                        } 
-                        else bestfr = fr;
-                        
-                        if (wr == -1) {
-                            double d2 = (double) map.get("wr");
-                            System.out.println("d2: " + d2);
-                            //bestwr = (int) map.get("wr");
-                            bestwr = (int) (d2 * 100);
-                            System.out.println("bestwr: " + bestwr);
-                        } else bestwr = wr;
+                        bestFile = file.getName();
+                        bestfr = fr;
+                        bestwr = wr;
                     }
                 }
                 
-                Stat stat = new Stat(cross, mutate);
+                Stat stat = new Stat(cross, mutate, fr, wr);
                 if (!stats.contains(stat)) { 
-                    stat.add((List<Integer>)map.get("gen"), (List<Integer>)map.get("time"));
+                    stat.add((List<Integer>)map.get("gen"), (List<Integer>)map.get("time"), (List<Double>)map.get("fitness"));
                     stats.add(stat); }
                 else {
                     for (Stat st : stats) {
                         if (st.equals(stat)) {
-                            st.add((List<Integer>)map.get("gen"), (List<Integer>)map.get("time"));
+                            st.add((List<Integer>)map.get("gen"), (List<Integer>)map.get("time"), (List<Double>)map.get("fitness"));
                         }
                     }
                 }
+                if (oldType) { fr = -1; wr = -1; }
             }catch (IOException ex) { log.info(ex);  }
+            if (oldType) { fr = -1; wr = -1; }
         }
         
         Map<String, Double> mapGen = new HashMap<>();
@@ -118,22 +117,26 @@ public class GAParse {
             log.info("Stat: " + st.toString());
             log.info("Sum: " + st.sumGen());
             log.info("Avg: " + st.avgGen());
-            mapGen.put(st.id(), st.avgGen());
+            mapGen.put(st.simpleid(), st.avgGen());
         }
         Map<String, Double> sortedMapGen = sortByComparator(mapGen);
         
         Map<String, Double> mapTime = new HashMap<>();
-        for (Stat st : stats) {
-            mapTime.put(st.id(), st.avgTime());
-        }
+        for (Stat st : stats) mapTime.put(st.id(), st.avgTime());
         Map<String, Double> sortedMapTime = sortByComparator(mapTime);
+        
+        Map<String, Double> mapFitness = new HashMap<>();
+        for (Stat st : stats) mapFitness.put(st.id(), st.avgFitness());
+        Map<String, Double> sortedMapFitness = sortByComparator(mapFitness);
         
         log.info("Map: " + mapGen);
         log.info("");
         log.info("Sorted by generation (from best to worst): "); printMap(sortedMapGen);
         log.info("");
         log.info("Sorted by time, sec (from best to worst): "); printMap(sortedMapTime);
-        log.info("Best fitness: " + bestFitness + ". Fr: " + bestfr + ". Wr: " + bestwr + ". File " + bestFile);
+        log.info("");
+        log.info("Sorted by fitness (from worst to best): "); printMap(sortedMapFitness);
+        log.info("Best fitness: " + bestFitness + ". Fr: " + bestfr + ". Wr: " + bestwr + ". File: " + bestFile);
     }
     
     public static File[] getFilesInDir(String path) {
@@ -168,37 +171,51 @@ public class GAParse {
     }
     
     public static void printMap(Map<String, Double> map){
-        for (String s : map.keySet()) { log.info(s + ": " + map.get(s)); }
+        log.info("Cross|Mutation|FreeRate|WasteRate|Fitness");
+        for (String s : map.keySet()) { log.info(s + "       | " + map.get(s)); }
     }
     
     // Чтение локального файла построчно
     public static Map read(String filename) throws IOException {
-        List<Integer> gen = new ArrayList<>();
+        List<Integer> generationIdx = new ArrayList<>();
         List<Integer> time = new ArrayList<>();
         List<Double> fitness = new ArrayList<>();
         double fr = -1d;
         double wr = -1d;
+        double bestFitness = 0d;
+        int generation = 0;
 
         BufferedReader in = new BufferedReader(new FileReader(filename));
         while (in.ready()) {
             String s = in.readLine();
-            if (s.startsWith("2."))  gen.add(Integer.valueOf(s.substring(s.indexOf(" ")).trim()));
+            //if (s.startsWith("2."))  generationIdx.add(Integer.valueOf(s.substring(s.indexOf(" ")).trim()));
+            // парсим время
             if (s.startsWith("Exec time:")) {
                 String[] minSec = s.substring(11).trim().replaceAll("m", "").replaceAll("sec", "").split(" ");
-                //System.out.println("Min:" + minSec[0]);
-                //System.out.println("Sec:" + minSec[1]);
                 int total = Integer.valueOf(minSec[0]) * 60 + Integer.valueOf(minSec[1]);
-                //System.out.println("Total:" + total);
                 time.add(total);
             }
-            if (s.startsWith("Fitness"))  fitness.add(Double.valueOf(s.substring(s.indexOf("=") + 1).trim()));
-            if (s.startsWith("Mutate free rate"))  fr = Double.valueOf(s.substring(s.indexOf(":") + 1).trim());
-            if (s.startsWith("Mutate waste rate"))  wr = Double.valueOf(s.substring(s.indexOf(":") + 1).trim());
+            
+            // парсим пары значений фитнес поколение и выбираем запоминаем лучий фитнес и поколение на котором он появился
+            if (s.length() > 2 && s.charAt(1) == '.') {
+                String[] arr = s.split(" ");
+                double fit = Double.valueOf(arr[0]);
+                int gen = Integer.valueOf(arr[1]);
+                if (fit > bestFitness) {
+                    bestFitness = fit;
+                    generation = gen;
+                }
+            }
+            //if (s.startsWith("Fitness"))  fitness.add(Double.valueOf(s.substring(s.indexOf("=") + 1).trim()));
+            if (s.startsWith("Mutate free rate")) fr = Double.valueOf(s.substring(s.indexOf(":") + 1).trim());
+            if (s.startsWith("Mutate waste rate")) wr = Double.valueOf(s.substring(s.indexOf(":") + 1).trim());
         }
         in.close();
-        
+        generationIdx.add(generation);
+        fitness.add(bestFitness);
+
         Map map = new HashMap<>();
-        map.put("gen", gen);
+        map.put("gen", generationIdx);
         map.put("time", time);
         map.put("fitness", fitness);
         map.put("fr", fr);
--- GAParse/src/gaparse/Stat.java
+++ GAParse/src/gaparse/Stat.java
@@ -10,17 +10,20 @@ import java.util.List;
 public class Stat {
     private int cross;
     private int mutate;
+    private int freeRate;
+    private int wasteRate;
     private List<Integer> generation = new ArrayList<>();
     private List<Integer> time = new ArrayList<>();
+    private List<Double> fitness = new ArrayList<>();
 
-    public Stat(int cr, int mut) { cross = cr; mutate = mut; }
-    public Stat(int cr, int mut, int gen) {
-        cross = cr;
-        mutate = mut;
-        generation.add(gen);
-    }
+    public Stat(int cr, int mut, int fr, int wr) { cross = cr; mutate = mut;  freeRate = fr; wasteRate = wr; }
+//    public Stat(int cr, int mut, int gen) {
+//        cross = cr;
+//        mutate = mut;
+//        generation.add(gen);
+//    }
 
-    public void add(List<Integer> g, List<Integer> t) { generation.addAll(g); time.addAll(t);}
+    public void add(List<Integer> g, List<Integer> t, List<Double> f) { generation.addAll(g); time.addAll(t); fitness.addAll(f); }
     public int sumGen() { 
         int sum = 0;
         for (Integer i : generation) { sum += i; }
@@ -31,18 +34,28 @@ public class Stat {
         for (Integer i : time) { sum += i; }
         return sum;
     }
+    
+    public double sumFitness() { 
+        double sum = 0d;
+        for (Double i : fitness) { sum += i; }
+        return sum;
+    }
 
     public double avgGen() { return (double)sumGen() / generation.size(); }
     public double avgTime() { return (double)sumTime() / time.size(); }
+    public double avgFitness() { return (double)sumFitness() / fitness.size(); }
     public int size() { return generation.size(); }
 
-    @Override public String toString() { return id() + ": " + generation.size() + "(" + generation + ")"; }
-    public String id() { return cross + "/" + mutate; }
+    @Override public String toString() { return simpleid() + ": " + generation.size() + "(" + generation + ")"; }
+    public String simpleid() { return cross + "|" + mutate + "|" + freeRate + "|" + wasteRate; }
+    public String id() { return cross + "   |" + mutate + "       |" + freeRate + "      |" + wasteRate; }
 
     @Override public int hashCode() {
         int hash = 5;
         hash = 17 * hash + this.cross;
         hash = 17 * hash + this.mutate;
+        hash = 17 * hash + this.freeRate;
+        hash = 17 * hash + this.wasteRate;
         return hash;
     }
 
@@ -52,6 +65,8 @@ public class Stat {
         final Stat other = (Stat) obj;
         if (this.cross != other.cross) { return false; }
         if (this.mutate != other.mutate) { return false; }
+        if (this.freeRate != other.freeRate) { return false; }
+        if (this.wasteRate != other.wasteRate) { return false; }
         return true;
     }
 }
